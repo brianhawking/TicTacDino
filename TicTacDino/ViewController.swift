38134f7a4006440753753e1af70dcccd4b82ee6a
Brian Veitch <beveitch@gmail.com>
2022-01-15 15:58:54 -0600

    Initial Commit

---
+++ TicTacDino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacDino
+//
+//  Created by Brian Veitch on 1/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
