fa4f7730c03ff09d147e6225f139e5f38535c0b4
= <beveitch@gmail.com>
2022-01-16 16:26:25 -0600

    Update ViewController.swift

--- TicTacDino/ViewController.swift
+++ TicTacDino/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.title = "Main branch"
+        self.title = "main branch"
     }
 
 
