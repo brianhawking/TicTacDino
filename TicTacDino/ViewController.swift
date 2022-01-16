793e7ef875910821145a482338b9378a826e1933
= <beveitch@gmail.com>
2022-01-16 16:45:48 -0600

    update title

--- TicTacDino/ViewController.swift
+++ TicTacDino/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         self.title = "OH BOY"
+        view.backgroundColor = .blue
+        
+        
     }
 
 
